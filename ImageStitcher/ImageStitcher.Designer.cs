25a39698adefcd27adb94abd2be0ad55eae4f9c4
Jonathan Bao <jbao161@gmail.com>
2018-10-10 00:18:48 -0400

    first working version

---
+++ ImageStitcher/ImageStitcher.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ImageStitcher
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_bothimages = new System.Windows.Forms.Panel();
+            this.splitContainer_bothimages = new System.Windows.Forms.SplitContainer();
+            this.pictureBox_leftpanel = new System.Windows.Forms.PictureBox();
+            this.pictureBox_rightpanel = new System.Windows.Forms.PictureBox();
+            this.panel_controls = new System.Windows.Forms.Panel();
+            this.button_preview = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel_bothimages.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer_bothimages)).BeginInit();
+            this.splitContainer_bothimages.Panel1.SuspendLayout();
+            this.splitContainer_bothimages.Panel2.SuspendLayout();
+            this.splitContainer_bothimages.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_leftpanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_rightpanel)).BeginInit();
+            this.panel_controls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_bothimages
+            // 
+            this.panel_bothimages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_bothimages.Controls.Add(this.splitContainer_bothimages);
+            this.panel_bothimages.Location = new System.Drawing.Point(0, 0);
+            this.panel_bothimages.Name = "panel_bothimages";
+            this.panel_bothimages.Size = new System.Drawing.Size(853, 615);
+            this.panel_bothimages.TabIndex = 0;
+            // 
+            // splitContainer_bothimages
+            // 
+            this.splitContainer_bothimages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer_bothimages.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer_bothimages.Name = "splitContainer_bothimages";
+            // 
+            // splitContainer_bothimages.Panel1
+            // 
+            this.splitContainer_bothimages.Panel1.Controls.Add(this.pictureBox_leftpanel);
+            // 
+            // splitContainer_bothimages.Panel2
+            // 
+            this.splitContainer_bothimages.Panel2.Controls.Add(this.pictureBox_rightpanel);
+            this.splitContainer_bothimages.Size = new System.Drawing.Size(853, 615);
+            this.splitContainer_bothimages.SplitterDistance = 303;
+            this.splitContainer_bothimages.SplitterWidth = 10;
+            this.splitContainer_bothimages.TabIndex = 0;
+            // 
+            // pictureBox_leftpanel
+            // 
+            this.pictureBox_leftpanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox_leftpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_leftpanel.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_leftpanel.Name = "pictureBox_leftpanel";
+            this.pictureBox_leftpanel.Size = new System.Drawing.Size(303, 615);
+            this.pictureBox_leftpanel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_leftpanel.TabIndex = 0;
+            this.pictureBox_leftpanel.TabStop = false;
+            this.pictureBox_leftpanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox_DragDrop);
+            this.pictureBox_leftpanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox_DragEnter);
+            // 
+            // pictureBox_rightpanel
+            // 
+            this.pictureBox_rightpanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox_rightpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_rightpanel.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_rightpanel.Name = "pictureBox_rightpanel";
+            this.pictureBox_rightpanel.Size = new System.Drawing.Size(540, 615);
+            this.pictureBox_rightpanel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_rightpanel.TabIndex = 0;
+            this.pictureBox_rightpanel.TabStop = false;
+            this.pictureBox_rightpanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox_DragDrop);
+            this.pictureBox_rightpanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox_DragEnter);
+            // 
+            // panel_controls
+            // 
+            this.panel_controls.Controls.Add(this.button_preview);
+            this.panel_controls.Controls.Add(this.button_save);
+            this.panel_controls.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel_controls.Location = new System.Drawing.Point(0, 621);
+            this.panel_controls.Name = "panel_controls";
+            this.panel_controls.Size = new System.Drawing.Size(853, 122);
+            this.panel_controls.TabIndex = 1;
+            // 
+            // button_preview
+            // 
+            this.button_preview.Location = new System.Drawing.Point(535, 1);
+            this.button_preview.Name = "button_preview";
+            this.button_preview.Size = new System.Drawing.Size(159, 120);
+            this.button_preview.TabIndex = 2;
+            this.button_preview.Text = "Preview";
+            this.button_preview.UseVisualStyleBackColor = true;
+            this.button_preview.Click += new System.EventHandler(this.button_preview_Click);
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(700, 3);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(150, 116);
+            this.button_save.TabIndex = 0;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 743);
+            this.Controls.Add(this.panel_controls);
+            this.Controls.Add(this.panel_bothimages);
+            this.Name = "MainWindow";
+            this.Text = "Image Stitcher";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.panel_bothimages.ResumeLayout(false);
+            this.splitContainer_bothimages.Panel1.ResumeLayout(false);
+            this.splitContainer_bothimages.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer_bothimages)).EndInit();
+            this.splitContainer_bothimages.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_leftpanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_rightpanel)).EndInit();
+            this.panel_controls.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_bothimages;
+        private System.Windows.Forms.Panel panel_controls;
+        private System.Windows.Forms.SplitContainer splitContainer_bothimages;
+        private System.Windows.Forms.PictureBox pictureBox_leftpanel;
+        private System.Windows.Forms.PictureBox pictureBox_rightpanel;
+        private System.Windows.Forms.Button button_preview;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ImageStitcher/ImageStitcher.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageStitcher
+{
+    public partial class MainWindow : Form
+    {
+
+        private bool debugflag = true;
+        private System.Windows.Forms.PictureBox this_picturebox;
+        private void debug(string message)
+        {
+            if (debugflag) Console.WriteLine(message);
+        }
+        int imgleft_height, imgleft_width, imgright_height, imgright_width, result_height, result_width = 0, splitter_position, panel_height, panel_width, leftpicturebox_width, rightpicturebox_width;
+        float imgsize_ratio, splittersize_ratio;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            pictureBox_rightpanel.AllowDrop = true;
+            pictureBox_leftpanel.AllowDrop = true;
+        }
+        private void pictureBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void pictureBox_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            this_picturebox = ((System.Windows.Forms.PictureBox)sender);
+            try
+            {
+                this_picturebox.Image = Bitmap.FromFile(s[0]);
+            }
+            catch (OutOfMemoryException ex)
+            {
+                Console.WriteLine("File is not a valid image: {0}. \n {1}", s[0], ex);
+                return;
+            }
+            // resize the splitcontainer
+            // * get the image dimensions and compare with splitcontainer dimensions
+            if (debugflag) Console.WriteLine("Image dimensions: {0} height and {1} width", this_picturebox.Image.Height, this_picturebox.Image.Width);
+
+        }
+
+        private Bitmap stitch_images()
+        {
+
+            if ((pictureBox_leftpanel.Image is null) || (pictureBox_rightpanel.Image is null))
+            {
+                debug("error. don't have both images loaded");
+                return null;
+            }
+            else
+            {
+                // resize the taller image to the height of the smaller image
+                imgleft_height = pictureBox_leftpanel.Image.Height;
+                imgleft_width = pictureBox_leftpanel.Image.Width;
+                imgright_height = pictureBox_rightpanel.Image.Height;
+                imgright_width = pictureBox_rightpanel.Image.Width;
+                result_height = Math.Min(imgleft_height, imgright_height);
+
+                if (imgleft_height > result_height)
+                {
+                    this_picturebox = pictureBox_leftpanel;
+                    imgsize_ratio = (float)result_height / (float)imgleft_height;
+                    this_picturebox.Image = new Bitmap(this_picturebox.Image,
+                    (int)(imgleft_width * imgsize_ratio), result_height);
+                    imgleft_width = this_picturebox.Image.Width;
+
+                }
+                else
+                {
+                    this_picturebox = pictureBox_rightpanel;
+                    imgsize_ratio = (float)result_height / (float)imgright_height;
+                    this_picturebox.Image = new Bitmap(this_picturebox.Image,
+                    (int)(imgright_width * imgsize_ratio), result_height);
+                    imgright_width = this_picturebox.Image.Width;
+                }
+                result_width = imgleft_width + imgright_width;
+                Bitmap stitchedimage = new Bitmap(result_width, result_height);
+                using (Graphics g = Graphics.FromImage(stitchedimage))
+                {
+                    g.DrawImage(pictureBox_leftpanel.Image, 0, 0);
+                    g.DrawImage(pictureBox_rightpanel.Image, imgleft_width, 0);
+                }
+                return stitchedimage;
+            }
+        }
+        private void button_preview_Click(object sender, EventArgs e)
+        {
+            Bitmap stitchedimage = stitch_images();
+            if (!(stitchedimage is null)) try
+                {
+                    using (Form form = new Form())
+                    {
+
+                        form.StartPosition = FormStartPosition.CenterScreen;
+                        form.Size = stitchedimage.Size;
+
+                        PictureBox pb = new PictureBox();
+                        pb.Dock = DockStyle.Fill;
+                        pb.Image = stitchedimage;
+                        pb.SizeMode = PictureBoxSizeMode.Zoom;
+                        form.Controls.Add(pb);
+                        form.ShowDialog();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("{0}", ex);
+                }
+        }
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            Bitmap stitchedimage = stitch_images();
+            if (!(stitchedimage is null)) try
+                {
+                    // Displays a SaveFileDialog so the user can save the Image   
+                    saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png";
+                    saveFileDialog1.Title = "Save an Image File";
+                    saveFileDialog1.RestoreDirectory = true;
+                    saveFileDialog1.ShowDialog();
+
+                    // If the file name is not an empty string open it for saving.  
+                    if (saveFileDialog1.FileName != "")
+                    {
+                        // Saves the Image via a FileStream created by the OpenFile method.  
+                        System.IO.FileStream fs =
+                           (System.IO.FileStream)saveFileDialog1.OpenFile();
+                        // Saves the Image in the appropriate ImageFormat based upon the  
+                        // File type selected in the dialog box.  
+                        // NOTE that the FilterIndex property is one-based.  
+                        switch (saveFileDialog1.FilterIndex)
+                        {
+                            case 1:
+                                stitchedimage.Save(fs,
+                                   System.Drawing.Imaging.ImageFormat.Jpeg);
+                                break;
+
+                            case 2:
+                                stitchedimage.Save(fs,
+                                   System.Drawing.Imaging.ImageFormat.Bmp);
+                                break;
+
+                            case 3:
+                                stitchedimage.Save(fs,
+                                   System.Drawing.Imaging.ImageFormat.Gif);
+                                break;
+                            case 4:
+                                stitchedimage.Save(fs,
+                                   System.Drawing.Imaging.ImageFormat.Png);
+                                break;
+                        }
+
+                        fs.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("{0}", ex);
+                }
+        }
+    }
+}
+
+
