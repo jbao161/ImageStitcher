295c00a89db2216e14419182ed103c67cbb38ebf
Jon <jbao161@gmail.com>
2022-11-30 11:44:16 -0800

    in progress: settings file corruption handling and image cropping

--- ImageStitcher/ImageStitcher.cs
+++ ImageStitcher/ImageStitcher.cs
@@ -111,7 +111,7 @@ namespace ImageStitcher
             pictureBox_leftpanel.AllowDrop = true;
 
             string configPathBackup; //https://stackoverflow.com/questions/42708868/user-config-corruption
-            try
+        /*    try
             {
                 Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
                 configPathBackup = config.FilePath + ".bak";
@@ -136,7 +136,11 @@ namespace ImageStitcher
                 }
                 Properties.Settings.Default.Reload();
                 //_logger.Error("Config file {0} does not exist", filename);
-            }
+            }*/
+            try
+            {// https://www.codeproject.com/Articles/30216/Handling-Corrupt-user-config-Settings
+                Settings.Default.Reload();
+
 
             //https://www.codeproject.com/Articles/15013/Windows-Forms-User-Settings-in-C
             // Set window location
@@ -158,6 +162,33 @@ namespace ImageStitcher
             this.checkBox_openaftersave.Checked = Settings.Default.OpenFolderAfterSave;
 
             tmpAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\ImageStitcher\\tmp\\";
+            }
+            catch (ConfigurationErrorsException ex)
+            { //(requires System.Configuration)
+                string filename = ((ConfigurationErrorsException)ex.InnerException).Filename;
+
+                if (System.Windows.MessageBox.Show("<ProgramName> has detected that your" +
+                                      " user settings file has become corrupted. " +
+                                      "This may be due to a crash or improper exiting" +
+                                      " of the program. <ProgramName> must reset your " +
+                                      "user settings in order to continue.\n\nClick" +
+                                      " Yes to reset your user settings and continue.\n\n" +
+                                      "Click No if you wish to attempt manual repair" +
+                                      " or to rescue information before proceeding.",
+                                      "Corrupt user settings",
+                                      System.Windows.MessageBoxButton.YesNo,
+                                      System.Windows.MessageBoxImage.Error) == System.Windows.MessageBoxResult.Yes)
+                {
+                    File.Delete(filename);
+                    Settings.Default.Reload();
+                    // you could optionally restart the app instead
+
+                    Application.Restart();
+                }
+                else
+                    Process.GetCurrentProcess().Kill();
+                // avoid the inevitable crash
+            }
         }
 
         private void PictureBox_DragEnter(object sender, DragEventArgs e)
@@ -659,7 +690,7 @@ namespace ImageStitcher
 
             // https://superuser.com/a/1256459 ffmpeg .mp4 to gif
 
-            String converttogif = $" && ffmpeg -y -i \"{tmpfilepath}\" -filter_complex \"fps=10,scale=iw:ih:flags=lanczos, split [o1] [o2];[o1] palettegen=stats_mode=diff [p]; [o2] fifo [o3];[o3] [p] paletteuse=dither=floyd_steinberg \" \"{fileOutPath}\"";
+            String converttogif = $" && ffmpeg -y -i \"{tmpfilepath}\" -filter_complex \"fps=10,scale=iw:ih:flags=lanczos, split [o1] [o2];[o1] palettegen=stats_mode=diff [p]; [o2] fifo [o3];[o3] [p] paletteuse=dither=sierra2_4a \" \"{fileOutPath}\"";
             string outputvisibility = "/C ";
             // use "/C "+ for cmd.exe to close automatically "/K "+ for cmd.exe to stay open and view ffmpeg output 
             string arg = outputvisibility + ifstillimagepresent + joinasvideo + converttogif + deletetempfiles;
@@ -1651,15 +1682,7 @@ namespace ImageStitcher
             try //https://stackoverflow.com/questions/42708868/user-config-corruption
             {
                 ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
-            }
-            catch (ConfigurationErrorsException exception)
-            {
-                Console.WriteLine("Settings are Corrupt!!");
-                // handle error or tell user
-                // to manually delete the file, you can do the following:
-                File.Delete(exception.Filename); // this can throw an exception too, so be wary!
-                Settings.Default.Reload();
-            }
+
 
             // Copy window location to app settings
             Settings.Default.WindowLocation = this.Location;
@@ -1680,6 +1703,22 @@ namespace ImageStitcher
 
             // Save settings
             Settings.Default.Save();
+            }
+            catch (ConfigurationErrorsException exception)
+            {
+                Console.WriteLine("Settings are Corrupt!!");
+                // handle error or tell user
+                // to manually delete the file, you can do the following:
+                try
+                {
+                    // File.Delete(exception.Filename); // this can throw an exception too, so be wary!
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                //Settings.Default.Reload();
+            }
             clearTmpAppData();
         }
 
