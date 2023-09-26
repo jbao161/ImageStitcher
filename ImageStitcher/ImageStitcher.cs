20d6ba7089056656ec2299c7edb2f7c24432c5b9
Jonathan Bao <jbao161@gmail.com>
2023-09-26 00:39:50 -0700

    better image resolution when zooming in and out

--- ImageStitcher/ImageStitcher.cs
+++ ImageStitcher/ImageStitcher.cs
@@ -1692,7 +1692,7 @@ namespace ImageStitcher
                 int picY = (containerheight - picheight) / 2;
 
                 targetpicturebox.Size = new Size(picwidth, picheight);
-                targetpicturebox.Image = new Bitmap(targetpicturebox.Image, new Size(picwidth, picheight));
+                //targetpicturebox.Image = new Bitmap(targetpicturebox.Image, new Size(picwidth, picheight));
                 // https://stackoverflow.com/questions/9375588/keeping-a-picturebox-centered-inside-a-container
                 targetpicturebox.Dock = DockStyle.None;
                 targetpicturebox.Location = new Point(picX, picY);
